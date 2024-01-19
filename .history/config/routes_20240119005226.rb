1ab16d469488b2a7e7a7d066177ea3d79c8aa479
bonneville <jeanlucbonneville@gmail.com>
2024-01-20 00:55:52 +0100

    routes.rb ajout de resources :home, only: [:index]

--- .history/config/routes_20240119005226.rb
+++ .history/config/routes_20240119005226.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :home, only: [:index]
-
+  root "home#index"
   get "home", to: "home#index"
 
   get '/contact', to: 'contact#index'
   post '/submit', to: 'contact#submit', as: 'submit_contact'
-
-  #root "home#index"
-
 end
