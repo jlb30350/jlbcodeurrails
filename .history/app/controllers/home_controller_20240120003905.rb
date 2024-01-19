0f569e778c1cbd02f008f2c3bbc2ea61735a750e
bonneville <jeanlucbonneville@gmail.com>
2024-01-20 00:40:38 +0100

    home controller a eté reinstallét

---
+++ .history/app/controllers/home_controller_20240120003905.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+    def index
+      # Le contenu de votre action
+    end
+  
+    # Assurez-vous que vous avez terminé la déclaration des actions correctement
+  end
+  
