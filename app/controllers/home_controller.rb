bdf9721bedb439e2588a1cb8fea17bfa0d92fa41
bonneville <jeanlucbonneville@gmail.com>
2024-01-20 23:25:55 +0100

    indexhtmlerb

--- app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-    def index.html.erb
-      # Le contenu de votre action
-    end
-  
-    # Assurez-vous que vous avez terminé la déclaration des actions correctement
-  end
-  
